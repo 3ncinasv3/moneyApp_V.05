6d63b8f52de91db5974a5f311f928173370a3646
3ncinasv3 <133629447+3ncinasv3@users.noreply.github.com>
2023-08-07 11:50:54 -0400

    first update with working customer and admin panel logic , can set balances. v.75

---
+++ src/Session.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+public class Session {
+  private static Session instance;
+  private final Customer customer;
+  private Session(Customer customer) {
+    this.customer = customer;
+  }
+  private static Customer customerSession(String username, String password) {
+    ArrayList<Customer> customers = FileHandle.readCustomers();
+    for (Customer value : customers) {
+      if (value.getUsername().equals(username) && value.getPassword().equals(password)) {
+        return value;
+      }
+    }
+    return null;
+  }
+  public static Session getInstance(String username, String password) {
+    if (instance == null) {
+      instance = new Session(customerSession(username, password));
+    }
+    return instance;
+  }
+  public Customer getCustomer() {
+    return this.customer;
+  }
+  public void cleanUserSession() {
+    instance = null;
+  }
+  public static Session getInstance() {
+    return instance;
+  }
+  public static void updateCustomerBalance(String username, String accountNumber, double newBalance) {
+    ArrayList<Customer> customers = FileHandle.readCustomers();
+    for (Customer customer : customers) {
+      if (customer.getUsername().equals(username) && customer.getAccountNumber().equals(accountNumber)) {
+        customer.setBalance(newBalance);
+        FileHandle.writeCustomersToFile(customers); // Write updated customer data to file
+        break;
+        }
+      }
+    }
+}
